6e7e415feb3bcd6f93cc1ceeb37e5a19fe312793
Jacob Vietorisz <jacobvietorisz@Jacobs-MacBook-Air-5.local>
2023-04-18 22:36:58 -0400

    Added code to load data from image folders and process them appropriately

--- load_data.py
+++ load_data.py
@@ -1,60 +1,100 @@
 import numpy as np
-from skimage import io, img_as_float32, transform
 from matplotlib import pyplot as plt
+from torchvision import datasets, transforms
+import torch
+import os
+import random
 
 # Load and preprocess data here
-
-def load_data():
-    # Return DataLoader? Not sure
-    return None
-
-
-def format_images(img_paths):
+def convert_file_paths(dataset_type, class_label):
     '''
-    Takes a set of images and formats them to be read by the descriminator network.
-    Formatted images are RGB with dtype float32 and shape (3, img_size, img_size)
+    Converts file paths of the form './datasets/<train or test>/<cat or dog>.xxx.jpg' into
+    './datasets/<train or test>/<real or fake>/<cat or dog>.xxx.jpg' so that they can be 
+    parsed by the Pytorch DataLoader object. 
+    Can be revised and used for adding more data in the future (such as pictures of birds)
+    The folders titled 'real' and 'fake' define the class assignments.
+
+    :params: 
+    dataset_type: string of 'train' or 'test'. Title of the folder whose images are to be further sorted 
+    class_label: string of 'real' or 'fake'. Title of the folder to which the images are moved
+    
+    :returns: 
+    None
+    '''
+    # Convert path names from cat.N.jpg to ./cat/N.jpg
+    directory = f'./datasets/{dataset_type}'
+    char_num = len(directory)
+    # iterate over files in that directory
+    for filename in os.listdir(directory):
+        file = os.path.join(directory, filename)
+        # Checking if it is a file. Will exclude created or existing folders
+        if os.path.isfile(file):
+            new_path = f'./datasets/{dataset_type}/{class_label}/{file[char_num:]}'
+            os.rename(file, new_path)
+        else:
+            pass
+
+    return
+
+
+
+def load_data(dataset_type, class_label):
+    '''
+    Loads data from a specified directory and reformats it. Each element in the returned list is a tuple,
+    (img, label)
 
     :params:
-    imgs_paths: a 1D np.array (or python list) of local directories for the real image set
-
-    :returns:
-    formatted_imgs: an np.ndarray of shape [num_imgs, 3, 64, 64]
+    dataset_type: string of 'train' or 'test'. Title of the folder from which to load data 
+    class_label: string of 'real' or 'fake'. Title of the folder from which to load data
+    
+    :returns: 
+    data: a python list, where each element is a tuple of an image(np.ndarray of shape (3, 64, 64)) 
+          and its label (np.ndarray of shape (1) with element 0 or 1. 0 --> real; 1 --> fake)
     '''
-    img_size = 64 # number of pixels per side in output images
-    num_imgs = len(img_paths)
+    data_set = f'./datasets/{dataset_type}/{class_label}'
+    transform = transforms.Compose([transforms.Resize(64),
+                                 transforms.CenterCrop(64),
+                                 transforms.ToTensor()])
+    training_dataset = datasets.ImageFolder(data_set, transform=transform)
+    data_loader = torch.utils.data.DataLoader(training_dataset, batch_size=1, shuffle=True)
 
-    # Initialize the output array
-    formatted_imgs = np.zeros([num_imgs, 3, img_size, img_size])
-
-    # Loop through images and format it
-    for i in range(num_imgs):
-        img = io.imread(img_paths[i])
-        img = img_as_float32(img)
-        if len(img.shape) == 2: # Convert BW images to RGB
+    data = []
+    # Populate data array with images and their labels
+    for img, label in data_loader:
+        img = np.squeeze(img)
+        label = np.asarray(label)
+        # Check and convert BW --> RGB
+        if len(img.shape) == 2:
             img = np.stack([img, img, img], axis=-1)
-        img = transform.resize(img, (3, 64, 64), anti_aliasing=True)
-        formatted_imgs[i] = img
+        # Add image and label as tuple to the data list
+        data.append((img, label))
 
-    return formatted_imgs
+    return data
 
 
 
 def shuffle_real_and_fake(real_imgs, fake_imgs):
     '''
-    Creates a single, randomly ordered array of images from two arrays of real and fake images, respectively.
+    Creates a single, randomly ordered list of image and label tuples from two lists of 
+    real and fake images, respectively.
 
     :params:
-    real_imgs: a np.ndarray of shape [n, 3, img_size, img_size] composed of real images from the data set
-    fake_imgs: a np.ndarray of shape [m, 3, img_size, img_size] produced by the generator network
+    real_imgs: a list of length n composed of tuples of real images from the data set and their labels: (img, label)
+    fake_imgs: a list of length m composed of tuples of real images from the data set and their labels: (img, label)
 
     :returns:
-    random_img_shuffle: a np.ndarray of shape [n+m, img_size, img_size, 3] of randomly ordered real and fake images
-    '''
+    img_shuffle: an np.ndarray of shape [n+m, 3, 64, 64] of randomly ordered real and fake images
+    label_shuffle: an np.ndarray of shape [n+m] of the corresponding label for images in random_img_shuffle
 
-    random_img_shuffle = np.append(real_imgs, fake_imgs, axis=0)
-    np.random.shuffle(random_img_shuffle)
+    '''
+    # Concatenate lists
+    composite = real_imgs + fake_imgs
+    reordered = random.shuffle(composite)
+    # Unpack tuples
+    img_shuffle = np.array([reordered[i][0] for i in range(len(composite))])
+    label_shuffle = np.array([reordered[i][1] for i in range(len(composite))])
     
-    return random_img_shuffle
+    return img_shuffle, label_shuffle
 
 
 
@@ -74,7 +114,7 @@ def standardize(imgs):
     return
 
 
-def create_generator_seeds(batch_size):
+def create_generator_seeds(batch_size, as_image):
     '''
     Creates a new set of random seed vectors for the generator
 
@@ -84,6 +124,10 @@ def create_generator_seeds(batch_size):
     :returns:
     generator_seeds: an np.ndarray of shape [batch_size, 1, 100] with random entries on [0,1]
     '''
-    generator_seeds = np.random.rand(batch_size, 1, 100)
+    if as_image==True:
+        generator_seeds = np.random.rand(batch_size, 3, 64, 64)
+    
+    else:
+        generator_seeds = np.random.rand(batch_size, 1, 100)
 
     return generator_seeds
