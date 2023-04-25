8b44a47335d4fb9242b6a89bf4878447721d1bed
syamamo1 <45343031+syamamo1@users.noreply.github.com>
2023-04-25 15:35:36 -0400

    update run file

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@
 # PYTHON = python environment
 # TRAIN = training file
 
-export PYTHON=/home/syamamo1/anaconda3/envs/timesformer/bin/python
-export TRAIN=/home/syamamo1/course/cs1430/final_project/main.py
+export PYTHON=/home/syamamo1/anaconda3/envs/petgen/bin/python
+export TRAIN=/home/syamamo1/course/cs1430/theRealPetGenerator/main.py
 
 
 $PYTHON $TRAIN
